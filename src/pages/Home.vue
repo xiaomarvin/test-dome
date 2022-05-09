6b025f615883bcb95b48bc9582d644a354335049
marvin <xiaomarvin9527@gmail.com>
2022-05-10 01:48:43 +0800

    init2

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -27,14 +27,6 @@
         :breakpoint="500"
         content-class="bg-grey-3"
       >
-        <q-scroll-area class="fit">
-          <div class="q-pa-sm">
-            <div
-              v-for="n in 50"
-              :key="n"
-            >Drawer {{ n }} / 50</div>
-          </div>
-        </q-scroll-area>
       </q-drawer>
       <!-- 侧边栏 end -->
     </q-toolbar>
