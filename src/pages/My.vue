3db1d1529cb833d2887b865a67ca4770e876ceb6
marvin <xiaomarvin9527@gmail.com>
2022-05-15 00:33:18 +0800

    bob

--- src/pages/My.vue
+++ src/pages/My.vue
@@ -1,16 +1,28 @@
 <template>
-  <!-- <q-page class="flex flex-center">
-    index页面
-    <router-view />
-  </q-page> -->
-  <div>
-    <q-btn>My</q-btn>
+<div>
+ <div class="row justify-between">
+      <q-input
+      class="col"
+        outlined
+        v-model.lazy="textuser"
+        label="用户名"
+        placeholder="请输入数字..."
+      />
+      <q-btn>搜索</q-btn>
+    </div>
+    
+    <div>{{ textuser }}</div>
   </div>
-
+  
 </template>
 
 <script>
 export default {
-  name: 'My'
+  name: 'My',
+  data(){
+    return{
+      textuser:""
+    }
+  }
 }
 </script>
