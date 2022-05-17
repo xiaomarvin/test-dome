17da17f787596d40bd8b3477a269ddf104926eef
marvin <xiaomarvin9527@gmail.com>
2022-05-17 23:53:19 +0800

    mark4

--- src/pages/Movie.vue
+++ src/pages/Movie.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="">
-    <q-btn>Movie</q-btn>
+    <q-btn-dropdown dense-toggle dense flat label="音乐播放器" class="q-mr-md">
+      <q-item v-close-popup to="/home" clickable v-ripple>
+        <q-item-section>返回主页</q-item-section>
+      </q-item>
+      <q-item to="/index" clickable v-ripple>
+        <q-item-section>视屏爆发</q-item-section>
+      </q-item>
+      <q-item to="/imde" clickable v-ripple>
+        <q-item-section>Single line item</q-item-section>
+      </q-item>
+    </q-btn-dropdown>
   </div>
 </template>
 
 <script>
 export default {
   name: "Movie",
-  data () {
-    return {
-    };
+  data() {
+    return {};
   },
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
