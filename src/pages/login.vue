0b7f5aef9156e33fc59ee7e93982831f11a530c8
marvin <xiaomarvin9527@gmail.com>
2022-05-26 23:18:57 +0800

    login.vue

---
+++ src/pages/login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">登录页面</div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  methods: {},
+};
+</script>
+
+<style scoped lang="less"></style>
