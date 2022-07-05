e42ea4d4a735bb50966226eb73725e9d4b8f6ede
marvin <xiaomarvin9527@gmail.com>
2022-07-06 01:06:00 +0800

    ssss

---
+++ src/pages/dome.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div>dome1</div>
+    <div>dome2</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  methods: {},
+};
+</script>
+
+<style scoped lang="less"></style>
