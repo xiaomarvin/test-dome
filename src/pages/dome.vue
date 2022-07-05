7e1849e111e67b78e3a99e0bc94ece7b8609f380
marvin <xiaomarvin9527@gmail.com>
2022-07-06 00:55:00 +0800

    dome3

--- src/pages/dome.vue
+++ src/pages/dome.vue
@@ -1,1 +1,20 @@
-dome dome2
+<template>
+  <div class="container">dome1</div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  methods: {},
+};
+</script>
+
+<style scoped lang="less"></style>
